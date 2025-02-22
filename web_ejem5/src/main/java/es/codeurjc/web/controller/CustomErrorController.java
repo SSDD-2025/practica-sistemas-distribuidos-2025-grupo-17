65b88936484a1a6c1594e32a665261a56afa9ffe
Zaira Ruiz Fernández <z.ruiz.2022@alumnos.urjc.es>
2025-02-22 12:57:48 +0100

    New error screen

---
+++ web_ejem5/src/main/java/es/codeurjc/web/controller/CustomErrorController.java
@@ -0,0 +1,16 @@
+package es.codeurjc.web.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        // Retorna el nombre de la plantilla de error personalizada
+        return "error_template";
+    }
+
+}
