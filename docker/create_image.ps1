173fcb9eba4c04b36ad7dfa57e5fc01286b5170a
tempusfugit04 <mtorresjuarez60@gmail.com>
2025-05-02 21:37:08 +0200

    Added create_image.ps1 and little modify in Dockerfile with path

---
+++ docker/create_image.ps1
@@ -0,0 +1,12 @@
+#Variables
+$dockerfile = "./docker/Dockerfile"
+$dockerHubUser = "tempusfugit04"
+$appName = "practica_grupo17"
+$version = "1.0.0"
+$imageName = "${dockerHubUser}/${appName}:${version}"
+
+# Creating image using Dockerfile
+Write-Host "Construyendo imagen Docker: $imageName ..."
+docker build -f ./docker/Dockerfile -t $imageName .
+
+Write-Host "Imagen construida correctamente: $imageName"
